178b1212ddeaed8611de16355175aca83c0301f3
Aaron <aaron@aaronkondziela.com>
2016-05-06 20:49:15 -0400

    updated hexdump files

--- src/hexdump.c
+++ src/hexdump.c
@@ -1,4 +1,36 @@
-// a hexdumper by Aaron Kondziela <aaron@aaronkondziela.com>
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2016 Aaron Kondziela <aaron@aaronkondziela.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+ /*
+  * Part of hexdump - Simple C function to print some memory as a
+  * standard hexdump
+  *
+  * https://github.com/aaronkondziela/hexdump/
+  *
+  * hexdump.c
+  *
+  */
 
 #include <stdio.h>
 
@@ -13,7 +45,7 @@ void hexdump(const void* d, size_t size)
 		printf("%02X ", data[i]);
 		txt[i % 16] = (data[i] >= 0x20 && data[i] <= 0x7E) ? data[i] : '.';
 		if ((i+1) % 8 == 0 || i+1 == size) printf(" ");
-		if (i+1 == size && (i+1) % 16 != 0 ) {
+		if (i+1 == size && (i+1) % 16 != 0) {
 			txt[(i+1) % 16] = '\0';
 			for (j = (i+1) % 16; j < 16; j++) printf("   ");
 			if ((i+1) % 16 <= 8) printf(" ");
